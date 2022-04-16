ea4a9da21683b6f20f87a96c2bfcf48d6c1c850e
Havilash <92213955+Havilash@users.noreply.github.com>
2022-04-16 12:13:10 +0200

    Import Project

---
+++ ConwaysGameOfLife/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConwaysGameOfLife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConwaysRules gameOfLife = new ConwaysRules();
+
+            //gameOfLife.gridBool[5, 5] = true;
+            
+
+
+            gameOfLife.StartGame();
+        }
+    }
+}
